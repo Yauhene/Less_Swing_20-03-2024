957ca55e3e1308655eeca5e9ec0af314f18ba6cb
Yauheni Aviltsau <xopek-007@yandex.ru>
2024-09-21 13:30:34 +0300

    Buttons are added in GameWindow.java

--- src/main/java/GameWindow.java
+++ src/main/java/GameWindow.java
@@ -7,10 +7,17 @@ public class GameWindow extends JFrame {
     private  static final int WINDOW_POSX = 800;
     private  static final int WINDOW_POSY = 300;
 
+    JButton btnStart = new JButton("New Game");
+    JButton btnExit = new JButton("Exit");
+
     GameWindow() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(WINDOW_POSX,WINDOW_POSY);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setTitle("TicTacToe");
+        setResizable(false);
+        add(btnStart);
+        add(btnExit);
 
         setVisible(true);
     }
