271e519ee7cd0b00789228497ec5c4e390e2fd85
Yauheni Aviltsau <xopek-007@yandex.ru>
2024-09-21 12:46:56 +0300

    The Start.java class has been created

--- src/main/java/GameWindow.java
+++ src/main/java/GameWindow.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
-    @Override
-    public void setDefaultCloseOperation(int operation) {
-        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    private  static final int WINDOW_HEIGHT = 555;
+    private  static final int WINDOW_WIDTH = 587;
+    private  static final int WINDOW_POSX = 800;
+    private  static final int WINDOW_POSY = 300;
+
+    GameWindow() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(WINDOW_POSX,WINDOW_POSY);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        setVisible(true);
     }
 }
