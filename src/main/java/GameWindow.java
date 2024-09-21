738607bf7b52648ee011b2a44606c3bdbb5d795a
Yauheni Aviltsau <xopek-007@yandex.ru>
2024-09-21 11:58:00 +0300

    The GameWindow class has been created

---
+++ src/main/java/GameWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    @Override
+    public void setDefaultCloseOperation(int operation) {
+        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
